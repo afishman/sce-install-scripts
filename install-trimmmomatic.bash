a2797b998d4de6492142b60c8f094f5769681e11
Aaron Fishman <uk.afishman@gmail.com>
2021-01-18 14:47:04 +0000

    trimmomatic

---
+++ install-trimmmomatic.bash
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install openjdk-11-jdk
+
+# Trimmomatic
+wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.38.zip
+unzip Trimmomatic-0.38.zip
+rm -f Trimmomatic-0.38.zip
