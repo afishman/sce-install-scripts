9a04c8651bf2391d6cd147f5b9b713af543baabe
Aaron Fishman <uk.afishman@gmail.com>
2021-01-18 14:55:53 +0000

    bracken

---
+++ install-bracken.bash
@@ -0,0 +1,9 @@
+# bracken
+wget https://github.com/jenniferlu717/Bracken/archive/v2.6.0.tar.gz
+tar xzf v2.6.0.tar.gz
+rm -f v2.6.0.tar.gz
+
+cd Bracken-2.6.0
+sh ./install_bracken.sh ../bracken
+
+sudo ln -s $PWD/../bracken/Bracken-2.6.0/bracken /usr/local/bin/bracken
